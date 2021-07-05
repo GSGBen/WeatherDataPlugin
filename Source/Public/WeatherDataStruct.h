5da458a7fa7a8d45d974e02e7e7cff70e2c292d8
Ben Renninson <ben@goldensyrupgames.com>
2021-07-05 19:28:44 +1000

    get it compiling on 4.24

--- Source/Public/WeatherDataStruct.h
+++ Source/Public/WeatherDataStruct.h
@@ -28,45 +28,45 @@ struct FWeatherDataStruct
  
 
 	/* Normalized global wind direction */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "WeatherDataPlugin")
 	FVector WindDirection;
 
 	/* Wind speed in km/h, (Multiply by 27.7778f  to convert to UE4 cm/s) */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "WeatherDataPlugin")
 	float WindSpeed;
 
 	/* Max wind gust speed in Km/h  */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "WeatherDataPlugin")
 	float WindGustSpeed;
 
 
 	/* Normalized global Sun Direction. If Z is less that 0 then sun is bellow horizon */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "WeatherDataPlugin")
 	FVector SunDirection;
 
 	/* Air humidity as a percentage, from 0 - 100% */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere,meta = (ClampMin = 0, ClampMax = 100, UIMin = 0, UIMax = 100))
+	UPROPERTY(BlueprintReadWrite, EditAnywhere,meta = (ClampMin = 0, ClampMax = 100, UIMin = 0, UIMax = 100), Category = "WeatherDataPlugin")
 	int32 Humidity;
 
 	/*  Air Temperature in celsius at Sealevel */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ClampMin = -100, ClampMax = 150, UIMin = -100, UIMax = 150))
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ClampMin = -100, ClampMax = 150, UIMin = -100, UIMax = 150), Category = "WeatherDataPlugin")
 	float SeaLevelTemperature;
 
 	/* Temp at which air can become water saturated, if air temp is at this point and comes in contact with a cooler surface then condensation/dew will form*/
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ClampMin = -100, ClampMax = 150, UIMin = -100, UIMax = 150))
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ClampMin = -100, ClampMax = 150, UIMin = -100, UIMax = 150), Category = "WeatherDataPlugin")
 	float DewPoint;
 
 
 	/* Rate of Precipitation (rain/snow fall rate) in 0-100 . Probably need a manual override control for it */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ClampMin = 0, ClampMax = 100, UIMin = 0, UIMax = 100))
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ClampMin = 0, ClampMax = 100, UIMin = 0, UIMax = 100), Category = "WeatherDataPlugin")
 	float PrecipitationRate;
 
 	/* Air pressure in hpa, Placeholder incase we need it  */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ClampMin = 0, ClampMax = 2000, UIMin = 0, UIMax = 2000))
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ClampMin = 0, ClampMax = 2000, UIMin = 0, UIMax = 2000), Category = "WeatherDataPlugin")
 	float AirPressure;
 
 	/* View distance in KM, relates to air clarity   */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ClampMin = 0, ClampMax = 200, UIMin = 0, UIMax = 200))
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ClampMin = 0, ClampMax = 200, UIMin = 0, UIMax = 200), Category = "WeatherDataPlugin")
 	float VisibilityDistance;
 
 
